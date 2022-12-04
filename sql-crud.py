fac162644615dc281815d8e9e2961138c31c1cb4
Grainne Donegan <grainnejdonegan@gmail.com>
2022-12-04 14:08:07 +0000

    update and delete the chinook database

--- sql-crud.py
+++ sql-crud.py
@@ -76,6 +76,14 @@ tim_berners_lee = Programmer(
     famous_for="World Wide Web"
 )
 
+grainne_donegan = Programmer(
+    first_name="Grainne",
+    last_name="Donegan",
+    gender="F",
+    nationality="Irish",
+    famous_for="Learning How to Code"
+)
+
 # add each instance of our programmers to our session
 # session.add(ada_lovelace)
 # session.add(alan_turing)
@@ -83,9 +91,43 @@ tim_berners_lee = Programmer(
 # session.add(margaret_hamilton)
 # session.add(bill_gates)
 # session.add(tim_berners_lee)
+# session.add(grainne_donegan)
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=9).first()
+# programmer.famous_for = "World President"
+# commit every change after the code every time
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# # defensive programming
+# if programmer is not None:
+#     print(f'Programmer found: {programmer.first_name} {programmer.last_name}')
+#     confirmation = input("Are you sure you want to delete this record: ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
 
 # commit out session to the db
-session.commit()
+# session.commit()
 
 # query the database to find all programmers
 programmers = session.query(Programmer)
